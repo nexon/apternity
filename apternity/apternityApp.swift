bc594a07776dd04b4c6fd7c16b548a65a45554eb
Alberto Lagos <alberto.lagos@gmail.com>
2023-07-16 09:31:47 -0400

    Initial Commit

---
+++ apternity/apternityApp.swift
@@ -0,0 +1,20 @@
+//
+//  apternityApp.swift
+//  apternity
+//
+//  Created by Alberto Lagos on 16-07-23.
+//
+
+import SwiftUI
+
+@main
+struct apternityApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
